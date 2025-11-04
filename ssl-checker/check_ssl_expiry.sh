f5698b4b12244273adb5aed0491951de32c155d0
valhio <valentingerasimow@gmail.com>
2025-11-04 18:08:01 +0200

    fix: ensure domains file existence check before processing and update resources used

--- ssl-checker/check_ssl_expiry.sh
+++ ssl-checker/check_ssl_expiry.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+domains_file="domains.txt"
+
+if [[ ! -f "$domains_file" ]]; then
+  echo "Error: File '$domains_file' not found."
+  exit 1
+fi
+
 while read domain; do
     echo "Checking SSL certificate for $domain ..."
     
@@ -10,14 +17,16 @@ while read domain; do
     	echo "  ❌ Failed to retrieve certificate for $domain"
  		continue
   	fi
-	
+
     date_s=$(date -d "${res}" +%s)
     now_s=$(date -d now +%s)
     days_left=$(( (date_s - now_s) / 86400 ))
     echo "$domain certificate will expire in $days_left days"
 
-done <domains.txt
+done < "$domains_file"
 
 # resources used:
-# https://stackoverflow.com/questions/1521462/looping-through-the-content-of-a-file-in-bash
-# https://askubuntu.com/questions/1198619/bash-script-to-calculate-remaining-days-to-expire-ssl-certs-in-a-website
+# https://stackoverflow.com/questions/1521462/looping-through-the-content-of-a-file-in-bash # reading file line by line
+# https://askubuntu.com/questions/1198619/bash-script-to-calculate-remaining-days-to-expire-ssl-certs-in-a-website # date calculations
+# https://stackoverflow.com/questions/13509508/check-if-string-is-neither-empty-nor-space-in-shell-script # string empty check
+# https://stackoverflow.com/questions/59579505/need-help-to-write-shell-script-to-check-if-file-exist-or-not # file existence check
